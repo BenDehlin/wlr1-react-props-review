b3a2aaeaf64907e120d08a383496137df9b188b9
Benjamin Dehlin <ben.dehlin@gmail.com>
2021-04-26 10:10:27 -0600

    add and edit

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,73 @@
-import React from 'react'
+// import React from 'react'
 
-function Movie(props){
-  console.log(props)
-  return(
-    <div style={{backgroundColor: 'pink'}}>
-      <h2>TITLE: {props.movie.title}</h2>
-      <h3>DIRECTOR: {props.movie.director}</h3>
-      <button onClick={() => props.deleteMovie(props.index)}>DELETE</button>
-    </div>
-  )
+// function Movie(props){
+//   console.log(props)
+//   return(
+//     <div style={{backgroundColor: 'pink'}}>
+//       <h2>TITLE: {props.movie.title}</h2>
+//       <h3>DIRECTOR: {props.movie.director}</h3>
+//       <button onClick={() => props.deleteMovie(props.index)}>DELETE</button>
+//     </div>
+//   )
+// }
+
+// export default Movie
+
+import React, { Component } from "react"
+
+class Movie extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      editMode: false,
+      title: "",
+      director: "",
+    }
+  }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  toggleEditMode = () => {
+    this.setState({ editMode: !this.state.editMode })
+  }
+
+  saveMovie = () => {
+    this.props.saveMovie(this.props.index, this.state.title, this.state.director)
+    this.toggleEditMode()
+  }
+
+  render() {
+    const { movie, deleteMovie, index } = this.props
+    return this.state.editMode ? (
+      <div>
+        <input
+          placeholder={movie.title}
+          value={this.state.title}
+          name="title"
+          onChange={(e) => this.handleChange(e)}
+        />
+        <input
+          placeholder={movie.director}
+          value={this.state.director}
+          name="director"
+          onChange={(e) => this.handleChange(e)}
+        />
+        <button onClick={this.saveMovie}>Save Movie</button>
+      </div>
+    ) : (
+      <div style={{ backgroundColor: "pink" }}>
+        <h2>TITLE: {movie.title}</h2>
+        <h3>DIRECTOR: {movie.director}</h3>
+        <button onClick={() => deleteMovie(index)}>DELETE</button>
+        <button onClick={this.toggleEditMode}>Edit</button>
+      </div>
+    )
+  }
 }
 
 export default Movie
--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import data from '../data.js'
 import Movie from './Movie'
+import Form from './Form'
 
 
 class MovieList extends Component{
@@ -19,15 +20,29 @@ class MovieList extends Component{
     })
   }
 
+  addMovie = (movie) => {
+    this.setState({
+      movies: [movie, ...this.state.movies]
+    })
+  }
+
+  saveMovie = (index, title, director) => {
+    const {movies} = this.state
+    movies.splice(index, 1, {title, director})
+    this.setState({movies})
+  }
+
   render(){
     return(
       <div>
         <h1>MovieList</h1>
+        <Form addMovie={this.addMovie} />
         {this.state.movies.map((element, index) => {
           return(
             <Movie 
             movie={element} 
-            deleteMovie={this.deleteMovie} 
+            deleteMovie={this.deleteMovie}
+            saveMovie={this.saveMovie} 
             index={index} />
           )
         })}
